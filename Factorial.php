9fec87f7b62551943005ab1de9dc5bc4311bea3d
MeRezaRezaei <77894240+MeRezaRezaei@users.noreply.github.com>
2021-07-07 14:09:02 +0430

    factorial calculation added

---
+++ Factorial.php
@@ -0,0 +1,13 @@
+<?php
+/* this program will calculate factorial of positive numbers
+ */
+ 
+ $number = 6; // the number you want to calculate its factorial
+ 
+ $fac = 1;
+ for ($i = 1;$i<= $number;$i++)
+ $fac = $fac * $i
+ ;
+ 
+ 
+ echo $fac;
