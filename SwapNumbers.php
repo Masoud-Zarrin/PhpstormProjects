d5fe14e4d9e54429cf9d2d3e2f75337688ed2a8b
MeRezaRezaei <77894240+MeRezaRezaei@users.noreply.github.com>
2021-07-08 23:49:05 +0430

    number swaper in php

---
+++ SwapNumbers.php
@@ -0,0 +1,15 @@
+<?php
+ // Write a Program to swap two numbers in PHP.
+ $number1 = 213;
+ $number2 = 756;
+ 
+ 
+ $temp = $number1;
+ 
+ $number1 = $number2;
+ 
+ $number2 = $temp;
+ 
+ echo '1) '.$number1.PHP_EOL.'2) '.$number2.PHP_EOL;
+ 
+ 
