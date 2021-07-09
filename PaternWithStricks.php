5dad2c5dae94bdd772f6e6fdf026a134187562cd
MeRezaRezaei <77894240+MeRezaRezaei@users.noreply.github.com>
2021-07-09 12:17:44 +0430

    paterns with stricks added

---
+++ PaternWithStricks.php
@@ -0,0 +1,18 @@
+<?php
+
+ /*
+  * Write a program for this Pattern:
+  *  ****
+    *     *
+    *     *
+    *     *
+  *  ****
+  * */
+
+echo ' **** '.PHP_EOL;
+
+for ($i = 1; $i <=10; $i++){
+ echo '*     *'.PHP_EOL;
+}
+
+echo ' **** '.PHP_EOL;
